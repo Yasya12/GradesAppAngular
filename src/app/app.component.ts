f61883bedc85ff2c420a30576af59cd0c3921d42
jasja <jasja200411@gmail.com>
2024-08-04 11:41:04 +0300

    Create project and first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'GradesAppAngular';
+export class AppComponent{
 }
+
